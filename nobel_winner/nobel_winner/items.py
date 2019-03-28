9b0e348108ffb89e795f08afae0b5e199e5bf393
watanabe <watanabe101watanabe@yahoo.co.jp>
2019-03-28 16:02:20 +0900

    year, category scrapyed

--- nobel_winner/nobel_winner/items.py
+++ nobel_winner/nobel_winner/items.py
@@ -12,4 +12,14 @@ class NobelWinnerItem(scrapy.Item):
     country = scrapy.Field()
     name = scrapy.Field()
     link_text = scrapy.Field()
+    category = scrapy.Field()
+    date_of_birth = scrapy.Field()
+    date_of_death = scrapy.Field()
+    gender = scrapy.Field()
+    link = scrapy.Field()
+    place_of_birth = scrapy.Field()
+    place_of_death = scrapy.Field()
+    text = scrapy.Field()
+    year = scrapy.Field()
+
     pass
--- nobel_winner/nobel_winner/spiders/nwinner_list_spider.py
+++ nobel_winner/nobel_winner/spiders/nwinner_list_spider.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from ..items import NobelWinnerItem
-
+import re
+BASE_URL = 'https://ja.wikipedia.org/wiki/'
 
 class NwinnerListSpiderSpider(scrapy.Spider):
     name = 'nwinner_list_spider'
@@ -15,10 +16,44 @@ class NwinnerListSpiderSpider(scrapy.Spider):
             if country:
                 winners = h4.xpath('following-sibling::ol[1]')
                 for w in winners.xpath('li'):
-                    text = w.xpath('descendant-or-self::text()').extract()
+
+                    wdata = process_winner_li(w, country[0])
                     yield NobelWinnerItem(
-                        country = country,
-                        name = text[0],
-                        link_text = ''.join(text)
+                        country=wdata['country'],
+                        name=wdata['name'],
+                        text=wdata['text'],
+                        year=wdata['year'],
+                        category=wdata['category'],
                     )
         pass
+
+
+
+def process_winner_li(w, country=None):
+    """受賞者の<li>タグを処理する"""
+    wdata = {}
+    wdata['link'] = BASE_URL + w.xpath('a/@href').extract()[0]
+    text = ''.join(w.xpath('descendant-or-self::text()').extract())
+    wdata['name'] = text.split('、')[0].strip()
+
+    year = re.findall('/d{4}', text) #4桁の文字列は年度である
+    if year:
+        wdata['year'] = int(year[0])
+    else:
+        wdata['year'] = 0
+        print('no year')
+
+    category = re.findall('文学賞|化学賞|物理学賞|生理学・医学賞|平和賞|経済学賞', text)
+    if category:
+        wdata['category'] = category[0]
+    else:
+        wdata['category'] = ''
+        print('no category')
+
+    if country:
+        wdata['country'] = country
+        wdata['born_in'] = ''
+
+    wdata['text'] = text
+    return wdata
+
